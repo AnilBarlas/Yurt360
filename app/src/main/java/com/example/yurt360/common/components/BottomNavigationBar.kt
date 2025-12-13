7984b544a3a72013dc35e464d90f460f8e1ad7f5
AnilBarlas <barlasanil@protonmail.com>
2025-12-13 21:45:08 +0300

    BottomNavagationBar

--- app/src/main/java/com/example/yurt360/common/components/BottomNavigationBar.kt
+++ app/src/main/java/com/example/yurt360/common/components/BottomNavigationBar.kt
@@ -22,7 +22,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
-
 @Composable
 fun CustomBottomNavigationBar(
     modifier: Modifier = Modifier,
