c73710209f425126b7fe6046bd7e8c659fc26e9a
yasinacar00 <yasin.acar@std.yeditepe.edu.tr>
2025-12-30 18:30:36 +0300

    Add Geologica font

---
+++ app/src/main/java/com/example/yurt360/common/utils/Fonts.kt
@@ -0,0 +1,10 @@
+package com.example.yurt360.common.utils
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.example.yurt360.R
+
+val Geologica = FontFamily(
+    Font(R.font.geologica, FontWeight.Normal)
+)
