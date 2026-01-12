1b4183f2b8d3752fcaa1022a88ce9a088bf9a6c6
yasinacar00 <yasin.acar@std.yeditepe.edu.tr>
2026-01-12 16:34:43 +0300

    Update AdminApplicationsViewModel.kt

--- app/src/main/java/com/example/yurt360/admin/changeRoom/AdminApplicationsViewModel.kt
+++ app/src/main/java/com/example/yurt360/admin/changeRoom/AdminApplicationsViewModel.kt
@@ -8,7 +8,7 @@ import androidx.compose.runtime.setValue      // IMPORT REQUIRED
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.yurt360.data.api.SupabaseClient
-import com.example.yurt360.model.ApplicationForm
+import com.example.yurt360.common.model.ApplicationForm
 import io.github.jan.supabase.postgrest.from
 import io.github.jan.supabase.postgrest.query.Columns
 import kotlinx.coroutines.flow.MutableStateFlow
